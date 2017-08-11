80bb5e934099f810365a9e4d87713b7024025152
JoaoBernardoo <joao@mail.uoguelph.ca>
2017-08-11 13:39:45 -0400

    collision in
    practice on invisible borders and collision on objects through tiled

---
+++ Assets/Maps/collision.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="red_collision" tilewidth="16" tileheight="16" spacing="1" tilecount="1" columns="1">
+ <image source="../Art/collision.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.5" y="0" width="16.75" height="16.375"/>
+  </objectgroup>
+ </tile>
+</tileset>
